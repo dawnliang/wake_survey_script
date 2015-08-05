930353ce832c9c7c4ef4175dcdf5511b662c6bd0
dawnliang <dawnliang@users.noreply.github.com>
2015-08-05 14:20:13 -0700

    Update calculate_surface_integral.m

--- calculate_surface_integral.m
+++ calculate_surface_integral.m
@@ -92,7 +92,7 @@ function r = calculate_surface_integral(X, Y, data)
         q = data(triangles(i,:));
         
         % interpolation & CoV
-        coeff = calcInterpCoeff(tri);
+        coeff = calculate_interpolation_coefficients(tri);
         % coeff = [a1 a2 a3;
         %          b1 b2 b3;
         %          c1 c2 c3]
